b8277cecb1d888f5bfc95ce7d66ba7907474f6a7
Miguel Diaz <miguel.diaz@thor.local>
2026-04-20 21:03:08 -0500

    Mesio 3

--- alembic/env.py
+++ alembic/env.py
@@ -60,7 +60,11 @@ def run_migrations_online() -> None:
     """Run migrations using a live synchronous connection."""
     connectable = create_engine(_database_url, poolclass=pool.NullPool)
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            transaction_per_migration=True,
+        )
         with context.begin_transaction():
             context.run_migrations()
 
